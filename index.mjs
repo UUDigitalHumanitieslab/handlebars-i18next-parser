faba1726a698aa5de8473a84a6532b0605f2d2b6
Julian Gonggrijp <dev@juliangonggrijp.com>
2022-03-29 19:19:19 +0200

    Add support for block helper syntax

--- index.mjs
+++ index.mjs
@@ -5,4 +5,15 @@ export default class HbsI18nLexer extends HandlebarsLexer {
         options.functions = options.functions || ['i18n'];
         super(options);
     }
+
+    createFunctionRegex() {
+        const funcName = this.functionPattern();
+        const anything = '([^]*?)';
+        const interpolation = `\\{\\{${funcName}\\s+${anything}}}`;
+        const blockOpen = `\\{\\{#${funcName}\\s+${anything}}}`;
+        const blockClose = `\\{\\{/${funcName}}}`;
+        const block = `${blockOpen}${anything}${blockClose}`;
+        const pattern = `${interpolation}|${block}`;
+        return this.functionRegex = new RegExp(pattern, 'gi');
+    }
 }
--- test.mjs
+++ test.mjs
@@ -58,4 +58,54 @@ describe('Lexer', function() {
     // In the original test suite, there was also a subsuite dedicated to the
     // `parseArguments` method. We did not copy this suite, because we inherit
     // the method without override.
+
+    // The remaining tests represent our custom behavior.
+
+    it('extracts keys from block helpers', function() {
+        assertExtract(this.lexer, "<p>{{#i18n 'first'}}{{/i18n}}</p>", [
+            {key: 'first'},
+        ]);
+    });
+
+    it('supports line breaks inside Handlebars interpolation tags', function() {
+        const content = "<p>{{i18n\n'first'}}</p>";
+        assertExtract(this.lexer, content, [{key: 'first'}]);
+    });
+
+    it('supports line breaks inside Handlebars block open tags', function() {
+        const content = "<p>{{#i18n\n'first'}}{{/i18n}}</p>";
+        assertExtract(this.lexer, content, [{key: 'first'}]);
+    });
+
+    it('supports line breaks between Handlebars block tags', function() {
+        const content = "<p>{{#i18n 'first'}}\n{{/i18n}}</p>";
+        assertExtract(this.lexer, content, [{key: 'first'}]);
+    });
+
+    it('extracts multiple blocks on a single line', function() {
+        const content = "<p>{{#i18n 'first'}}{{/i18n}}{{#i18n 'second'}}{{/i18n}}</p>";
+        assertExtract(this.lexer, content, [
+            {key: 'first'},
+            {key: 'second'},
+        ]);
+    });
+
+    it('supports custom function names for block helper tags', function() {
+        const lexer = new Lexer({functions: ['t']});
+        assertExtract(lexer, "<p>{{#t 'first'}}{{/t}}</p>", [{key: 'first'}]);
+    });
+
+    it('extracts keyword arguments from blocks', function() {
+        const content = "<p>{{#i18n 'first' lng='en'\ncontext='bla'}}{{/i18n}}</p>";
+        assertExtract(this.lexer, content, [{
+            key: 'first',
+            lng: 'en',
+            context: 'bla',
+        }]);
+    });
+
+    it('supports interpolation tags inside block default content', function() {
+        const content = "<p>{{#i18n 'first'}}default with {{interpolation}} tag{{/i18n}}</p>";
+        assertExtract(this.lexer, content, [{key: 'first'}]);
+    });
 });
